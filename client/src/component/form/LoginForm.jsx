476da439f445317ad126e719ac9afa03c44115e3
dwikoadya <dwikoadya@gmail.com>
2021-07-28 20:20:06 +0700

    Add Component Form Login

---
+++ client/src/component/form/LoginForm.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import { useForm } from 'react-hook-form'
+import Text from "../input/Text";
+import Password from "../input/Password";
+import Card from "../Card";
+
+export default function LoginForm() {
+  // const { register, handleSubmit } = useForm()
+  return (
+    <div className="container vh-100">
+      <div className="row vh-100">
+        <div className="col self-align-center">
+          <Card title="Login">
+            <form>
+              <Text label="Email" id="standard-basic" name="email" />
+              <Password label="Password" id="password" name="password" />
+              <button className="btn btn-primary" type="submit"> Login </button>
+            </form>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
