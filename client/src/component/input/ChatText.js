66485e3b0826f054b32966bd7b9f64d6fb0b507b
dwikoadya <dwikoadya@gmail.com>
2021-07-29 18:24:07 +0700

    Add Chat input Component

---
+++ client/src/component/input/ChatText.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ChatText() {
+  return (
+    <div className="speech-bubble col-8">
+      <div className="input-group">
+        <input
+          name="chat"
+          type="text"
+          placeholder="Write Your Chat Here..."
+          aria-describedby="button-addon2"
+          required={true}
+          className="form-control border-2 py-4 bg-light"
+        />
+      </div>
+    </div>
+  );
+}
