d5aac28ec8757944990f5b1570b4cee657fe1f48
dwikoadya <dwikoadya@gmail.com>
2021-07-29 18:25:30 +0700

    Fix Some Bugs

--- client/src/screens/Chat.jsx
+++ client/src/screens/Chat.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { useState } from "react";
+import ChatForm from "../component/form/ChatForm";
+// import { useState } from "react";
 
 export default function Chat() {
-  const [word, setWord] = useState("");
+  // const [word, setWord] = useState("");
 
   return (
     <div>
-      <p> { word } </p>
+      {/* <p> { word } </p>
       <input
         type="text"
         value={word}
         onChange={(e) => setWord(e.target.value)}
-      />
+      /> */}
+      <ChatForm />
     </div>
   );
 }
