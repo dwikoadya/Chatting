f91e4b4034f2a45de331fecc466a92d3c972f885
dwikoadya <dwikoadya@gmail.com>
2021-07-29 18:21:06 +0700

    Add Button Component

---
+++ client/src/component/button/SendButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SendButton() {
+  return (
+    <div className="speech-bubble">
+      <div className="input-group">
+        <button className="msg_send_btn" type="submit"><i className="fa fa-paper-plane" aria-hidden="true"></i> </button>
+      </div>
+    </div>
+  )
+}
