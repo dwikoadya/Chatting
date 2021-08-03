43dcb459c2c627151d4a3c7604b7d4ea49efa9dc
dwikoadya <dwikoadya@gmail.com>
2021-08-03 22:52:24 +0700

    Last Revision

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,7 +14,7 @@ router.get('/register', (req, res) => {
 })
 
 router.post("/register", (req, res) => {
-  let userName = req.body.userName;
+  let username = req.body.username;
   let email = req.body.email;
   let password = req.body.password;
   let retype = req.body.retype;
@@ -40,7 +40,7 @@ router.post("/register", (req, res) => {
       } else {
         var token = jwt.sign({ email: email }, secret);
         let user = new User({
-          userName: userName,
+          username: username,
           email: email,
           password: password,
           token: token,
@@ -48,11 +48,11 @@ router.post("/register", (req, res) => {
         user
           .save()
           .then((data) => {
-            console.log(data)
             (response.message = "Register Success"),
               (response.data.email = email,
-                response.data.userName = userName,
-                response.data.password = password);
+                response.data.username = username,
+                response.data.password = password
+              );
             response.token = token;
             res.status(200).json(response);
           })
